8ddf40bbf5e109df28137c9dcfb2c24af186e6cd
Khaos1324 <charles15851@gmail.com>
2022-02-14 19:56:51 -0800

    Added scene transition and interaction
    Added a way for the player to interact with an object that takes them to solve the puzzle. The player and the puzzle are at two different scenes and thus a scene transition has been added to make things go more smoothly.

---
+++ Galactic Education/Assets/LevelLoader.cs
@@ -0,0 +1,46 @@
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LevelLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Animator transition;
+
+    
+
+    public float transitionTime = 1f;
+
+    public void Update(){
+        
+    }
+
+    public void LoadSlidingPuzzle(){
+        
+        StartCoroutine(LoadLevel("SlidingPuzzle_Scene"));
+        
+        
+    }
+
+    public void LoadDistortClassroom(){
+        
+        StartCoroutine(LoadLevel("Distort Classroom"));
+        
+        
+    }
+
+    IEnumerator LoadLevel(string levelName){
+
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelName);
+
+
+    }
+
+}

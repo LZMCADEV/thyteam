9e72ed59f9cfca247b69f3497e6784e94eb9efdd
Khaos1324 <charles15851@gmail.com>
2022-03-01 21:37:36 -0800

    Added Animation
    Added the following animations to the player:
    - Idle Up
    - Idle Down
    - Idle Right
    - Idle Left
    - Walking Up
    - Walking Down
    - Walking Right
    - Walking left

--- Galactic Education V2/Galactic Education V2/Assets/Scripts/Player_Movement.cs
+++ Galactic Education V2/Galactic Education V2/Assets/Scripts/Player_Movement.cs
@@ -8,6 +8,9 @@ public class Player_Movement : MonoBehaviour
 {
     //This sets the movement speed for the player
     public float moveSpeed = 5f;
+    public Animator animator;
+
+    private string lastBool = "";
 
     //You're going to have to make a RigidBody2D component for the player and attach it to the variable rb.
     public Rigidbody2D rb;
@@ -31,7 +34,140 @@ public class Player_Movement : MonoBehaviour
         // Stores it in the vertical area
         movement.y = Input.GetAxisRaw("Vertical");
 
-    }
+        if ((Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.RightArrow)) ||  ((Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))) ){
+            animator.SetBool("Up", true);
+            animator.SetBool("Down", false);
+            animator.SetBool("Right", false);
+            animator.SetBool("Left", false);
+            animator.SetBool("Idle Up", false);
+            animator.SetBool("Idle Down", false);
+            animator.SetBool("Idle Right", false);
+            animator.SetBool("Idle Left", false);
+            lastBool = "Up";
+        } else if (Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.LeftArrow)  ||  ((Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))) ){
+            animator.SetBool("Up", true);
+            animator.SetBool("Down", false);
+            animator.SetBool("Right", false);
+            animator.SetBool("Left", false);
+            animator.SetBool("Idle Up", false);
+            animator.SetBool("Idle Down", false);
+            animator.SetBool("Idle Right", false);
+            animator.SetBool("Idle Left", false);
+            lastBool = "Up";
+        } else if (Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.DownArrow)  ||  ((Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.S))) ){
+            animator.SetBool("Up", true);
+            animator.SetBool("Down", false);
+            animator.SetBool("Right", false);
+            animator.SetBool("Left", false);
+            animator.SetBool("Idle Up", false);
+            animator.SetBool("Idle Down", false);
+            animator.SetBool("Idle Right", false);
+            animator.SetBool("Idle Left", false);
+            lastBool = "Up";
+        } else if (Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.RightArrow)  ||  ((Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))) ){
+            animator.SetBool("Up", false);
+            animator.SetBool("Down", true);
+            animator.SetBool("Right", false);
+            animator.SetBool("Left", false);
+            animator.SetBool("Idle Up", false);
+            animator.SetBool("Idle Down", false);
+            animator.SetBool("Idle Right", false);
+            animator.SetBool("Idle Left", false);
+            lastBool = "Down";
+        } else if (Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.LeftArrow)  ||  ((Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))) ){
+            animator.SetBool("Up", false);
+            animator.SetBool("Down", true);
+            animator.SetBool("Right", false);
+            animator.SetBool("Left", false);
+            animator.SetBool("Idle Up", false);
+            animator.SetBool("Idle Down", false);
+            animator.SetBool("Idle Right", false);
+            animator.SetBool("Idle Left", false);
+            lastBool = "Down";
+        } else if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.RightArrow)  ||  ((Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D))) ){
+            animator.SetBool("Up", false);
+            animator.SetBool("Down", true);
+            animator.SetBool("Right", false);
+            animator.SetBool("Left", false);
+            animator.SetBool("Idle Up", false);
+            animator.SetBool("Idle Down", false);
+            animator.SetBool("Idle Right", false);
+            animator.SetBool("Idle Left", false);
+            lastBool = "Down";
+        } else if (Input.GetKey(KeyCode.UpArrow)  ||  Input.GetKey(KeyCode.W) ){
+            animator.SetBool("Up", true);
+            animator.SetBool("Down", false);
+            animator.SetBool("Right", false);
+            animator.SetBool("Left", false);
+            animator.SetBool("Idle Up", false);
+            animator.SetBool("Idle Down", false);
+            animator.SetBool("Idle Right", false);
+            animator.SetBool("Idle Left", false);
+            lastBool = "Up";
+        } else if (Input.GetKey(KeyCode.DownArrow)  ||  Input.GetKey(KeyCode.S) ){
+            animator.SetBool("Up", false);
+            animator.SetBool("Down", true);
+            animator.SetBool("Right", false);
+            animator.SetBool("Left", false);
+            animator.SetBool("Idle Up", false);
+            animator.SetBool("Idle Down", false);
+            animator.SetBool("Idle Right", false);
+            animator.SetBool("Idle Left", false);
+            lastBool = "Down";
+        } else if (Input.GetKey(KeyCode.LeftArrow)  ||  Input.GetKey(KeyCode.A) ){
+            animator.SetBool("Up", false);
+            animator.SetBool("Down", false);
+            animator.SetBool("Right", false);
+            animator.SetBool("Left", true);
+            animator.SetBool("Idle Up", false);
+            animator.SetBool("Idle Down", false);
+            animator.SetBool("Idle Right", false);
+            animator.SetBool("Idle Left", false);
+            lastBool = "Left";
+        } else if (Input.GetKey(KeyCode.RightArrow)  ||  Input.GetKey(KeyCode.D) ){
+            animator.SetBool("Up", false);
+            animator.SetBool("Down", false);
+            animator.SetBool("Right", true);
+            animator.SetBool("Left", false);
+            animator.SetBool("Idle Up", false);
+            animator.SetBool("Idle Down", false);
+            animator.SetBool("Idle Right", false);
+            animator.SetBool("Idle Left", false);
+            lastBool = "Right";
+        } else {
+            animator.SetBool("Up", false);
+            animator.SetBool("Down", false);
+            animator.SetBool("Right", false);
+            animator.SetBool("Left", false);
+            if (lastBool == "Up"){
+                animator.SetBool("Idle Up", true);
+                animator.SetBool("Idle Down", false);
+                animator.SetBool("Idle Right", false);
+                animator.SetBool("Idle Left", false);
+            } else if (lastBool == "Down"){
+                animator.SetBool("Idle Up", false);
+                animator.SetBool("Idle Down", true);
+                animator.SetBool("Idle Right", false);
+                animator.SetBool("Idle Left", false);
+            } else if (lastBool == "Right"){
+                animator.SetBool("Idle Up", false);
+                animator.SetBool("Idle Down", false);
+                animator.SetBool("Idle Right", true);
+                animator.SetBool("Idle Left", false);
+            } else if (lastBool == "Left"){
+                animator.SetBool("Idle Up", false);
+                animator.SetBool("Idle Down", false);
+                animator.SetBool("Idle Right", false);
+                animator.SetBool("Idle Left", true);
+            }
+ 
+
+
+
+
+
+        }
+        }
 
     
 

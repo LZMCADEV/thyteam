f4112c5344701d39c4bf126bb456910906e7e725
Khaos1324 <charles15851@gmail.com>
2022-03-03 23:39:42 -0800

    Bug Fix
    - Fixed an animation bug where there were inconsistencies with the animations. This should now be fixed
    
    - [WIP] Adding sword animations

---
+++ Galactic Education V2/Galactic Education V2/Assets/PlayerCombat.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0)){
+            Attack();
+        }
+
+    }
+
+    void Attack(){
+        
+    }
+}
